4648cf5dbb808b6eb099f1967450e2a62b863a5b
Гуляев Денис Михайлович <Gulyaev_DM@irkutskoil.ru>
2025-05-24 20:55:53 +0800

    update code

--- students/models.py
+++ students/models.py
@@ -1,41 +1,19 @@
 from django.db import models
 
 
-class MyModel(models.Model):
-    name = models.CharField(max_length=300, blank=True)
-    description = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Student(models.Model):
-    FIRST_YEAR = 'first'
-    SECOND_YEAR = 'second'
-    THIRD_YEAR = 'third'
-    FOURTH_YEAR = 'fourth'
-
-    YEAR_IN_SCHOOL_CHOICES = [
-        (FIRST_YEAR, 'Первый курс'),
-        (SECOND_YEAR, 'Второй курс'),
-        (THIRD_YEAR, 'Третий курс'),
-        (FOURTH_YEAR, 'Четвертый курс'),
-    ]
-
-    first_name = models.CharField(max_length=150, verbose_name='Имя')
-    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
-    year = models.CharField(max_length=6, choices=YEAR_IN_SCHOOL_CHOICES, default=FIRST_YEAR, verbose_name='Курс')
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f"{self.first_name} {self.last_name}"
 
-    class Meta:
-        verbose_name = 'студент'
-        verbose_name_plural = 'студенты'
-        ordering = ['last_name']
-        permissions = [
-            ('can_promote_student', 'Can_promote_student'),
-            ('can_expel_student', 'Can_expel_student'),
-        ]
+
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100)
+    score = models.FloatField()
+
+    def __str__(self):
+        return f"{self.subject}: {self.score}"
--- students/views.py
+++ students/views.py
@@ -1,91 +1,19 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
-from django.views.generic import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from students.models import Student, MyModel
-from django.views.generic import ListView, DetailView, View
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseForbidden
+from django.views.generic import DetailView
+from .models import Student
+from .services import StudentService
 
-
-class ExpelStudentView(LoginRequiredMixin, View):
-    def post(self, request, student_id):
-        student = get_object_or_404(Student, id=student_id)
-
-        if not request.user.has_perm('students.can_expel_student'):
-            return HttpResponseForbidden('У вас нет прав для исключения студента')
-
-        student.delete()
-
-        return redirect('students:student_list')
-
-class StudentListView(LoginRequiredMixin, ListView):
+class StudentDetailView(DetailView):
     model = Student
-    template_name = "students/student_list.html"
-    context_object_name = 'students'
+    template_name = 'students/student_detail.html'
+    context_object_name = 'student'
 
-    def get_queryset(self):
-        if not self.request.user.has_perm('students.view_student')
-            return Student.objects.none()
-        return Student.objects.all()
-
-
-
-class MyModelCreateView(CreateView):
-    model = MyModel
-    fields = ['name', 'description']
-    template_name = 'students/mymodel_form.html'
-    success_url = reverse_lazy('students:mymodel_list')
-
-
-class MyModelDetailView(DetailView):
-    model = MyModel
-    template_name = 'students/mymodel_detail.html'
-    context_object_name = 'mymodel'
-
-
-class MyModelUpdateView(UpdateView):
-    model = MyModel
-    fields = ['name', 'description']
-    template_name = 'students/mymodel_form.html'
-    success_url = reverse_lazy('students:mymodel_list')
-
-class MyModelDeleteView(DeleteView):
-    model = MyModel
-    template_name = 'students/mymodel_confirm_delete.html'
-    success_url = reverse_lazy('students:mymodel_list')
-
-
-class MyModelListView(ListView):
-    model = MyModel
-    template_name = 'students/mymodel_list.html'
-    context_object_name = 'mymodels'
-
-
-def about(request):
-    return render(request, "students/about.html")
-
-
-def contact(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        message = request.POST.get("message")
-
-        return HttpResponse(f"Спасибо, {name}! Сообщение получено.")
-    return render(request, 'students/contact.html')
-
-
-def student_detail(request):
-    student = Student.objects.get(id=1)
-    context = {
-        'student': student,
-    }
-    return render(request, 'students/student_detail.html', context=context)
-
-
-def student_list(request):
-    students = Student.objects.all()
-    context = {
-        'students': students
-    }
-    return render(request, 'students/student_list.html', context=context)
+    def get_context_data(self, **kwargs):
+        # Получаем стандартный контекст данных из родительского класса
+        context = super().get_context_data(**kwargs)
+        # Получаем ID студента из объекта
+        student_id = self.object.id
+        # Добавляем в контекст полное имя, средний балл и статус сдачи предмета
+        context['full_name'] = StudentService.get_full_name(student_id)
+        context['average_grade'] = StudentService.calculate_average_grade(student_id)
+        context['has_passed'] = StudentService.has_passed(student_id)
+        return context
